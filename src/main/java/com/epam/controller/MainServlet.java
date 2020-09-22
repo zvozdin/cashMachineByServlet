4a7959ee30e00098457dcd649ca7ae7ff9e8c7d0
iblYmyck <zvyozdin@gmail.com>
2020-09-22 20:31:56 +0300

    first commit. hello world on jsp into browser

---
+++ src/main/java/com/epam/controller/MainServlet.java
@@ -0,0 +1,24 @@
+package com.epam.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = getAction(req);
+
+        if (action.startsWith("/")) {
+            req.getRequestDispatcher("helloWorld.jsp").forward(req, resp);
+        }
+    }
+
+    private String getAction(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        return requestURI.substring(req.getContextPath().length());
+    }
+}
---
+++ src/main/webapp/helloWorld.jsp
@@ -0,0 +1,9 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+    <head>
+        <title>cashMachineByServlet</title>
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>
